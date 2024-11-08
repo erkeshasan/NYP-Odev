a2412fe9bfe28e5ab1849347d955792558213707
Hasan <erkeshasan@gmail.com>
2024-11-09 01:02:47 +0300

    Update 4_2.cs

--- Odev-4/4_2.cs
+++ Odev-4/4_2.cs
@@ -1,1 +1,21 @@
+int[] intdizi = new int[] {-150, 150, 23, 4, 0, 90, 7300, 620};
 
+Console.WriteLine(Dizi.EnBüyük(intdizi));
+
+public static class Dizi
+{
+    public static double EnBüyük(int[] Dizi)
+    {
+        int enBüyük = Dizi[0];
+
+        foreach (int i in Dizi)
+        {
+            if (i > enBüyük)
+            {
+                enBüyük = i;
+            }
+        }
+
+        return enBüyük;
+    }
+}
