caec55ea98a444c14d355d19fc089ea643ad3ca5
Hasan <erkeshasan@gmail.com>
2024-11-09 00:28:07 +0300

    Update 4_1.cs

--- Odev-4/4_1.cs
+++ Odev-4/4_1.cs
@@ -1,1 +1,29 @@
+while (true)
+{
+    Console.Write("Üçgen Tabanı: ");
+    double taban = Convert.ToDouble(Console.ReadLine());
 
+    Console.Write("Üçgen Yüksekliği: ");
+    double yükseklik = Convert.ToDouble(Console.ReadLine());
+
+    Üçgen üçgen = new Üçgen(taban, yükseklik);
+
+    Console.Write("\nÜçgen Alanı: " + üçgen.Alan() + "\n\n");
+}
+
+class Üçgen
+{
+    public double Taban;
+    public double Yükseklik;
+
+    public Üçgen(double taban, double yükseklik)
+    {
+        Taban = taban;
+        Yükseklik = yükseklik;
+    }
+
+    public double Alan()
+    {
+        return (Taban * Yükseklik) / 2;
+    }
+}
