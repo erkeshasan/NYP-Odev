f5672e8d2d195a59312fc25ed869c82d1575c2ff
Hasan <erkeshasan@gmail.com>
2024-11-09 20:19:06 +0300

    Update 4_4.cs

--- Odev-4/4_4.cs
+++ Odev-4/4_4.cs
@@ -1,12 +1,9 @@
-Console.WriteLine(Matematik.Fibonacci(9));
+Console.WriteLine(Fibonacci(9));
 
-class Matematik
+static int Fibonacci(int Terim)
 {
-    public static int Fibonacci(int Terim)
-    {
-        if (Terim <= 1)
-            return Terim;
+    if (Terim <= 1)
+        return Terim;
 
-        return Fibonacci(Terim - 2) + Fibonacci(Terim - 1);
-    }
+    return Fibonacci(Terim - 2) + Fibonacci(Terim - 1);
 }
