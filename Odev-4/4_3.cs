923f1de7f9fa47e699cebcb26a9878e1b35e42ce
Hasan <erkeshasan@gmail.com>
2024-11-22 23:59:56 +0300

    Update 4_3.cs

--- Odev-4/4_3.cs
+++ Odev-4/4_3.cs
@@ -1,11 +1,5 @@
 class Program
 {
-    static void Main() {
-        Console.WriteLine(CalculateSum(20, 50));
-        Console.WriteLine(CalculateSum(20.6, 50.3));
-        Console.WriteLine(CalculateSum(20, 50, 30));
-    }
-
     public static int CalculateSum(int Bir, int İki)
     {
         return Bir + İki;
@@ -19,5 +13,12 @@ class Program
     public static int CalculateSum(int Bir, int İki, int Üç)
     {
         return Bir + İki + Üç;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(CalculateSum(20, 50));
+        Console.WriteLine(CalculateSum(20.6, 50.3));
+        Console.WriteLine(CalculateSum(20, 50, 30));
     }
 }
