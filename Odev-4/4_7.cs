0c057ad69625558abc96d1423ddb69ae388ae98b
Hasan <erkeshasan@gmail.com>
2024-11-23 00:12:22 +0300

    Update 4_7.cs

--- Odev-4/4_7.cs
+++ Odev-4/4_7.cs
@@ -1,11 +1,11 @@
 class Program
 {
-    static int YaşBüyüklüğü(int Yaş = 18)
+    public static int YaşBüyüklüğü(int Yaş = 18)
     {
         return Yaş - 18;
     }
 
-    static void Main()
+    public static void Main()
     {
         Console.WriteLine(YaşBüyüklüğü());
         Console.WriteLine(YaşBüyüklüğü(18));
