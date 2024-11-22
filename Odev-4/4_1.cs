13f655c58ae0570ee6941479b9ff0aff6d4fa868
Hasan <erkeshasan@gmail.com>
2024-11-22 19:13:03 +0300

    Update 4_1.cs

--- Odev-4/4_1.cs
+++ Odev-4/4_1.cs
@@ -1,29 +1,36 @@
-while (true)
+using System;
+
+class Program
 {
-    Console.Write("Üçgen Tabanı: ");
-    double taban = Convert.ToDouble(Console.ReadLine());
-
-    Console.Write("Üçgen Yüksekliği: ");
-    double yükseklik = Convert.ToDouble(Console.ReadLine());
-
-    Üçgen üçgen = new Üçgen(taban, yükseklik);
-
-    Console.Write("\nÜçgen Alanı: " + üçgen.Alan() + "\n\n");
-}
-
-class Üçgen
-{
-    public double Taban;
-    public double Yükseklik;
-
-    public Üçgen(double taban, double yükseklik)
+    static void Main()
     {
-        Taban = taban;
-        Yükseklik = yükseklik;
+        while (true)
+        {
+            Console.Write("Üçgen Tabanı: ");
+            double taban = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Üçgen Yüksekliği: ");
+            double yükseklik = Convert.ToDouble(Console.ReadLine());
+
+            Üçgen üçgenim = new Üçgen(taban, yükseklik);
+            Console.Write("\nÜçgen Alanı: " + üçgenim.Alan() + "\n\n\n");
+        }
     }
 
-    public double Alan() // Parametresiz metot?
+    class Üçgen
     {
-        return (Taban * Yükseklik) / 2;
+        public double Taban;
+        public double Yükseklik;
+
+        public Üçgen(double taban, double yükseklik)
+        {
+            Taban = taban;
+            Yükseklik = yükseklik;
+        }
+
+        public double Alan() // Parametresiz metot?
+        {
+            return (Taban * Yükseklik) / 2;
+        }
     }
 }
